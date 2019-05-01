4a6bf66cff858ce346bdea1493bac20da2ffa770
darlan <darlandc1998>
2019-05-01 09:03:16 -0300

    Lista de Ciclo de Pagamentos (Parte 1) #7

--- Frames/frontend/app/billingCycle/billingCycleController.js
+++ Frames/frontend/app/billingCycle/billingCycleController.js
@@ -7,16 +7,27 @@
 
   function BillingCycleController($http, msgs){
     const vm = this;
+    const url = 'http://localhost:3003/api/billingCycles';
+
+    vm.refresh = function (){
+      $http.get(url).then(function(response) {
+        vm.billingCycle = {}
+        vm.billingCycles = response.data;
+      }).catch(function(){
+
+      });
+    }
 
     vm.create = function(){
-      const url = 'http://localhost:3003/api/billingCycles';
       $http.post(url, vm.billingCycle).then(function(response){
-        vm.billingCycle = {};
+        refresh();
         msgs.addSuccess('Operação realizada com sucesso!');
       }).catch(function(response){
         msgs.addError(response.data.errors);
       });
     }
+
+    vm.refresh();
   }
 
 })()
